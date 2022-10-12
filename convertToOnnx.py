d82c92837bdfa98b307d287850a81cb5405a56df
Youssef Boulaoaune <43298428+Boulaouaney@users.noreply.github.com>
2022-10-12 10:00:04 +0900

    trained with resnet34 and ECA exportable block

--- convertToOnnx.py
+++ convertToOnnx.py
@@ -9,12 +9,25 @@ import models.resnet_last_down_extract as resnet_down
 parser = argparse.ArgumentParser(description='PyTorch Cifar10 Training with KD')
 parser.add_argument('--pair_keys', type=int, required=True,
                     help='---Indicate pair of keys unique for teacher and student---')
+parser.add_argument('--model', type=str, required=True, help='---Model type: resnet18, resnet34, resnet50---')
 args, unparsed = parser.parse_known_args()
 model_names = ['resnet18', 'resnet34', 'resnet50', 'resnet101', 'resnet152']
 
-torch_model = resnet_down.__dict__[model_names[0]]()
+def build_model():
+    if args.model == 'resnet18':
+        return resnet_down.__dict__[model_names[0]]()
+    elif args.model == 'resnet34':
+        return resnet_down.__dict__[model_names[1]]()
+    elif args.model == 'resnet50':
+        return resnet_down.__dict__[model_names[2]]()
+    elif args.model == 'resnet101':
+        return resnet_down.__dict__[model_names[3]]()
+    elif args.model == 'resnet152':
+        return resnet_down.__dict__[model_names[4]]()
 
-model_path = f'./vanilla_kd_model_saved_base/resnet18_student_{args.pair_keys}.pth'
+torch_model = build_model()
+
+model_path = f'./vanilla_kd_model_saved_base/{args.model}_student_{args.pair_keys}.pth'
 batch_size = 1
 
 map_location = lambda storage, loc: storage
@@ -32,7 +45,7 @@ torch_out = torch_model(x)
 
 torch.onnx.export(torch_model,
                   x,
-                  f"./vanilla_kd_ECA_ONNX_model/Student_model_{args.pair_keys}.onnx",
+                  f"./vanilla_kd_ECA_ONNX_model/{args.model}_Student_model_{args.pair_keys}.onnx",
                   export_params=True,
                   opset_version=10,
                   do_constant_folding=True,
