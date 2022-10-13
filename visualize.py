68c9f051b72d0417526cb6a327b261764faf15d8
Youssef Boulaoaune <43298428+Boulaouaney@users.noreply.github.com>
2022-10-13 19:10:58 +0900

    Update visualize.py

--- visualize.py
+++ visualize.py
@@ -3,31 +3,67 @@ from torchviz import make_dot
 import argparse
 import torch.onnx
 
-import models.resnet_ECA_parallel_SC as resnet_down
+import models.resnet_ECA_parallel_SC as resnet_ECA_parallel
+import models.resnet_last_down_extract as resnet_down_origin
+import models.resnet_ECA_last_block_SC as resnet_ECA_last
 import hiddenlayer as hl
 
 parser = argparse.ArgumentParser(description='PyTorch Cifar10 Training with KD')
 parser.add_argument('--pair_keys', type=int, required=True,
                     help='---Indicate pair of keys unique for teacher and student---')
 parser.add_argument('--model', type=str, required=True, help='---Model type: resnet18, resnet34, resnet50---')
+parser.add_argument('--ECA', type=str, default='yes', help='---model with ECA or without')
+parser.add_argument('--ECA_block', type=str, default='parallel', help='---ECA block in parallel or last')
 args, unparsed = parser.parse_known_args()
 model_names = ['resnet18', 'resnet34', 'resnet50', 'resnet101', 'resnet152']
 
 
-def build_model():
+def build_model_ECA_parallel():
     if args.model == 'resnet18':
-        return resnet_down.__dict__[model_names[0]]()
+        return resnet_ECA_parallel.__dict__[model_names[0]]()
     elif args.model == 'resnet34':
-        return resnet_down.__dict__[model_names[1]]()
+        return resnet_ECA_parallel.__dict__[model_names[1]]()
     elif args.model == 'resnet50':
-        return resnet_down.__dict__[model_names[2]]()
+        return resnet_ECA_parallel.__dict__[model_names[2]]()
     elif args.model == 'resnet101':
-        return resnet_down.__dict__[model_names[3]]()
+        return resnet_ECA_parallel.__dict__[model_names[3]]()
     elif args.model == 'resnet152':
-        return resnet_down.__dict__[model_names[4]]()
+        return resnet_ECA_parallel.__dict__[model_names[4]]()
 
 
-torch_model = build_model()
+def build_model_ECA_last():
+    if args.model == 'resnet18':
+        return resnet_ECA_last.__dict__[model_names[0]]()
+    elif args.model == 'resnet34':
+        return resnet_ECA_last.__dict__[model_names[1]]()
+    elif args.model == 'resnet50':
+        return resnet_ECA_last.__dict__[model_names[2]]()
+    elif args.model == 'resnet101':
+        return resnet_ECA_last.__dict__[model_names[3]]()
+    elif args.model == 'resnet152':
+        return resnet_ECA_last.__dict__[model_names[4]]()
+
+
+def build_model_origin():
+    if args.model == 'resnet18':
+        return resnet_down_origin.__dict__[model_names[0]]()
+    elif args.model == 'resnet34':
+        return resnet_down_origin.__dict__[model_names[1]]()
+    elif args.model == 'resnet50':
+        return resnet_down_origin.__dict__[model_names[2]]()
+    elif args.model == 'resnet101':
+        return resnet_down_origin.__dict__[model_names[3]]()
+    elif args.model == 'resnet152':
+        return resnet_down_origin.__dict__[model_names[4]]()
+
+
+if args.ECA == 'no':
+    torch_model = build_model_origin()
+elif args.ECA == 'yes':
+    if args.ECA_block == 'parallel':
+        torch_model = build_model_ECA_parallel()
+    elif args.ECA_block == 'last':
+        torch_model = build_model_ECA_last()
 
 model_path = f'./vanilla_kd_model_saved_base/{args.model}_student_{args.pair_keys}.pth'
 batch_size = 1
